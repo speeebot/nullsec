d023f9daf09300e07fd58774076cd42abc32411b
speeebot <speeebot@gmail.com>
2021-07-14 01:48:52 -0400

    added .gitignore

--- config/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('EMAIL', 'noreply@nullsec.gg');
-define('PASSWORD', '{k${Gf2)24%h');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'nullsec');
